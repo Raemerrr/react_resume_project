944cb544a152c2f936ef6a9935a9ed8a17df6e01
Raemin KANG <foals18@gmail.com>
2020-12-04 23:12:58 +0900

    글씨체 수정, About.js 부분 수정

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,9 +3,14 @@ import img from "../raemerrr.png";
 import "../styles/About.css";
 
 function About(props) {
-  const [aboutMe, aboutMeSet] = useState("기본 값");
+  const [aboutMe, aboutMeSet] = useState("저는 코딩을 좋아하고 즐기는 사람입니다.\
+  React에 관심이 생겨 해당 프로젝트를 진행하게 되었습니다.\
+  프론트엔트 엔지니어로 거듭나도록 하겠습니다.");
+
+  const [contact, ContactSet] =useState("🏡 경기도 안산시 단원구 광덕 1로 80. 📩 foals18@gmail.com");
   if (props.data) {
     aboutMeSet(props.data.adboutMe);
+    ContactSet(props.data.contactDetails);
   }
   return (
     <section id="about">
@@ -16,9 +21,11 @@ function About(props) {
       <div className="content">
         <div className="aboutMe">
           <h2>About Me</h2>
+          <h4>{aboutMe}</h4>
         </div>
         <div className="contactDetails">
-          <h4>{aboutMe}</h4>
+          <h2>Contact Details</h2>
+          <h4>{contact}</h4>
         </div>
       </div>
     </section>
