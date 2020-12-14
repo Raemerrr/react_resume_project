3de51de1ab000cd62e338955265b2792ff012916
Raemin KANG <foals18@gmail.com>
2020-12-14 21:25:05 +0900

    Resume.js CSS부분 수정

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -2,17 +2,36 @@ import React, { useState, useEffect } from "react";
 import "../styles/Resume.css";
 
 const Resume = (props) => {
+  const logoSrc = {
+    PYTHON:
+      "https://img.shields.io/badge/Python-3766AB?style=flat-square&logo=Python&logoColor=white",
+    JAVA:
+      "https://img.shields.io/badge/Java-007396?style=flat-square&logo=Java&logoColor=white",
+    "C++":
+      "https://img.shields.io/badge/C++-00599C?style=flat-square&logo=C%2B%2B&logoColor=white",
+    C:
+      "https://img.shields.io/badge/C-A8B9CC?style=flat-square&logo=C&logoColor=white",
+    JAVASCRIPT:
+      "https://img.shields.io/badge/Javascript-ffb13b?style=flat-square&logo=javascript&logoColor=white",
+    CSS:
+      "https://img.shields.io/badge/css-1572B6?style=flat-square&logo=css3&logoColor=white",
+    GO:
+      "https://img.shields.io/badge/Go-11B48A?style=flat-square&logo=Go&logoColor=white",
+    HTML:
+      "https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white",
+  };
+
   const [languages, setLanguages] = useState([
     <li key={1}>
-      <h1>{"C++"}</h1>
+      <img src={logoSrc['C++']} alt="missing" />
       <progress max={100} value={70} />
     </li>,
     <li key={2}>
-      <h1>{"C#"}</h1>
+      <img src={logoSrc.C} alt="missing" />
       <progress max={100} value={80} />
     </li>,
     <li key={3}>
-      <h1>{"JavaScript"}</h1>
+      <img src={logoSrc.JAVASCRIPT} alt="missing" />
       <progress max={100} value={55} />
     </li>,
   ]);
@@ -27,7 +46,7 @@ const Resume = (props) => {
       for (let k in data) {
         resumeDataList.push(
           <li key={resumeDataList.length}>
-            <h1>{k}</h1>
+            <img src={logoSrc[k.toUpperCase()]} alt={k.toUpperCase()} />
             <progress max={100} value={(data[k] / total) * 100} />
           </li>
         );
--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export { default as Home } from './Home';
 export { default as About } from './About';
 export { default as Resume } from './Resume';
 export { default as Works } from './Works';
+export { default as Contact } from './Contact';
